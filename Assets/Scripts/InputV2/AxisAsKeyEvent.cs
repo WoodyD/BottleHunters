d9d8ea8d8a4548a7b16a5baf6b66e4acff10de80
WOODY\Woody <yaroslav.dyatel@gmail.com>
2018-02-19 17:06:17 +0100

    New input System. First draft

---
+++ Assets/Scripts/InputV2/AxisAsKeyEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisAsKeyEvent{
+	public System.Action<bool> OnAxisEvent;
+	public float inputDeadZone = 0.2f;
+
+	private float axisInput;
+	public void GetAxisEvent (string axisName) {
+		axisInput = Input.GetAxis (axisName);
+		if (axisInput < -inputDeadZone || axisInput > inputDeadZone)
+			OnAxisEvent (true);
+	}
+}
---
+++ Assets/Scripts/InputV2/AxisEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisEvent {
+	public System.Action<float> OnAxisEvent;
+	public float inputDeadZone = 0.2f;
+
+	private float axisInput;
+	public void GetAxisEvent(string axisName) {
+		axisInput = Input.GetAxis (axisName);
+		if (axisInput < -inputDeadZone || axisInput > inputDeadZone)
+			OnAxisEvent (axisInput);
+	}
+}
