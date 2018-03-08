b0f4ee45225d92ff105d5bbc6718d045dfc87c25
Woody <yaroslav.dyatel@gmail.com>
2018-03-08 21:48:03 +0100

    Add DontDestroyOnLoad script/component

---
+++ Assets/Scripts/Systems/DontDestroyOnLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+    private static DontDestroyOnLoad gameComponent;
+
+	void Awake () {
+        
+		DontDestroyOnLoad(this);
+
+        if(gameComponent == null)
+            gameComponent = this;         
+        else
+            Destroy(this);
+        
+	}
+
+}
