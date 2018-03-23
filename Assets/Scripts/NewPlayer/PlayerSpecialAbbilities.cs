77c26dc0751df791f29889017ffe9fdfc089dd12
WOODY\Woody <yaroslav.dyatel@gmail.com>
2018-03-23 17:13:25 +0100

    Changes in player prefab. Network particle send. Not wokring exactly as expect but still.

---
+++ Assets/Scripts/NewPlayer/PlayerSpecialAbbilities.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpecialAbbilities : MonoBehaviour {
+
+	public bool isControllable = true;
+	public ParticleSystem burp;
+	public bool startBurp;
+
+	private void Update () {
+		if (isControllable) {
+			if (Input.GetKeyDown (KeyCode.E))
+				if (burp && !burp.isPlaying)
+					startBurp = true;
+			if (startBurp) {
+				MakeBurp ();
+			}
+		}
+	}
+
+	public void MakeBurp () {
+		burp.Play ();
+		startBurp = false;
+	}
+}
+
--- Assets/Scripts/NewPlayer/ThirdPersonNetwork.cs
+++ Assets/Scripts/NewPlayer/ThirdPersonNetwork.cs
@@ -1,79 +1,77 @@
 using UnityEngine;
 using System.Collections;
 
-public class ThirdPersonNetwork : Photon.MonoBehaviour
-{
-    ThirdPersonCamera cameraScript;
-    ThirdPersonController controllerScript;
+public class ThirdPersonNetwork : Photon.MonoBehaviour {
+	ThirdPersonCamera cameraScript;
+	ThirdPersonController controllerScript;
+	PlayerSpecialAbbilities specialAbbilities;
 
 	bool firstTake = false;
 
-	void OnEnable()
-	{
-	 	firstTake = true;	 
+	void OnEnable () {
+		firstTake = true;
 	}
 
-    void Awake()
-    {
-        cameraScript = GetComponent<ThirdPersonCamera>();
-        controllerScript = GetComponent<ThirdPersonController>();
+	void Awake () {
+		cameraScript = GetComponent<ThirdPersonCamera> ();
+		controllerScript = GetComponent<ThirdPersonController> ();
+		specialAbbilities = GetComponent<PlayerSpecialAbbilities> ();
 
-         if (photonView.isMine)
-        {
-            //MINE: local player, simply enable the local scripts
-            cameraScript.enabled = true;
-            controllerScript.enabled = true;
-        }
-        else
-        {           
-            cameraScript.enabled = false;
-			cameraScript.cameraTransform.gameObject.SetActive(false);
-			
-            controllerScript.enabled = true;
-            controllerScript.isControllable = false;
-        }
+		if (photonView.isMine) {
+			//MINE: local player, simply enable the local scripts
+			cameraScript.enabled = true;
+			controllerScript.enabled = true;
+			specialAbbilities.enabled = true;
+		} else {
+			cameraScript.enabled = false;
+			cameraScript.cameraTransform.gameObject.SetActive (false);
 
-        gameObject.name = gameObject.name + photonView.viewID;
-    }
+			controllerScript.enabled = true;
+			controllerScript.isControllable = false;
 
-    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.isWriting)
-        {
-            //We own this player: send the others our data
-            stream.SendNext((int)controllerScript._characterState);
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation); 
-        }
-        else
-        {
-            //Network player, receive data
-            controllerScript._characterState = (CharacterState)(int)stream.ReceiveNext();
-            correctPlayerPos = (Vector3)stream.ReceiveNext();
-            correctPlayerRot = (Quaternion)stream.ReceiveNext();
+			specialAbbilities.enabled = true;
+			specialAbbilities.isControllable = false;
+		}
+
+		gameObject.name = gameObject.name + photonView.viewID;
+	}
+
+	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) {
+			//We own this player: send the others our data
+			stream.SendNext ((int)controllerScript._characterState);
+			stream.SendNext (transform.position);
+			stream.SendNext (transform.rotation);
+			stream.SendNext ((bool)specialAbbilities.burp);
+		} else {
+			//Network player, receive data
+			controllerScript._characterState = (CharacterState)(int)stream.ReceiveNext ();
+			correctPlayerPos = (Vector3)stream.ReceiveNext ();
+			correctPlayerRot = (Quaternion)stream.ReceiveNext ();
+			specialAbbilities.startBurp = (bool)stream.ReceiveNext ();
+			Debug.Log (specialAbbilities.startBurp);
+			if(specialAbbilities.startBurp)
+				specialAbbilities.MakeBurp ();
 
 			// avoids lerping the character from "center" to the "current" position when this client joins
-			if (firstTake)
-			{
+			if (firstTake) {
 				firstTake = false;
 				this.transform.position = correctPlayerPos;
 				transform.rotation = correctPlayerRot;
 			}
 
-        }
-    }
+		}
+	}
 
-    private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
-    private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this
+	private Vector3 correctPlayerPos = Vector3.zero; //We lerp towards this
+	private Quaternion correctPlayerRot = Quaternion.identity; //We lerp towards this
 
-    void Update()
-    {
-        if (!photonView.isMine)
-        {
-            //Update remote player (smooth this, this looks good, at the cost of some accuracy)
-            transform.position = Vector3.Lerp(transform.position, correctPlayerPos, Time.deltaTime * 5);
-            transform.rotation = Quaternion.Lerp(transform.rotation, correctPlayerRot, Time.deltaTime * 5);
-        }
-    }
+	void Update () {
+		if (!photonView.isMine) {
+			//Update remote player (smooth this, this looks good, at the cost of some accuracy)
+			transform.position = Vector3.Lerp (transform.position, correctPlayerPos, Time.deltaTime * 5);
+			transform.rotation = Quaternion.Lerp (transform.rotation, correctPlayerRot, Time.deltaTime * 5);
+		}
+	}
 
 }
