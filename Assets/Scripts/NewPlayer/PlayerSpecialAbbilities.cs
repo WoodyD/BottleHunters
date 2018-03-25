2d2c27f42f6f59b013dcc26ae962228ec390717c
Woody <yaroslav.dyatel@gmail.com>
2018-03-25 15:42:13 +0200

    Changes in special abilities script on attack. Try to send special events through network.

--- Assets/Scripts/NewPlayer/PlayerSpecialAbbilities.cs
+++ Assets/Scripts/NewPlayer/PlayerSpecialAbbilities.cs
@@ -3,41 +3,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpecialAbbilities : Photon.MonoBehaviour, IPunObservable {
+public class Attack{	
+	public bool startBurp;
+}
+
+public class PlayerSpecialAbbilities : Photon.MonoBehaviour {
+	public ParticleSystem burp;
 
 	public bool isControllable = true;
-	public ParticleSystem burp;
-	public bool startBurp;
+	public Attack attackWithBurp;
 
+	private void OnEnable() {
+		PhotonNetwork.OnEventCall += this.CustomBurpEvent;
+	}
+
+	private void OnDisable() {
+		PhotonNetwork.OnEventCall -= this.CustomBurpEvent;
+	}
+	
 	private void Update () {
-		if (isControllable) {
+		if (isControllable)
 			if (Input.GetKeyDown(KeyCode.E))
-				TryBurp();
-		}else{
-			if (startBurp)
+				OnStartBurp();
+		//		attackWithBurp.TryBurp();
+		//}else{
+		//	Debug.Log("IsControllable by other player. Start burp? " + attackWithBurp.startBurp);
+		//	if (attackWithBurp.startBurp) 
+		//		attackWithBurp.MakeBurp();
+			
+		//}
+	}
+
+	private void OnStartBurp(){
+		TryBurp();
+		byte evCode = 0;
+		bool reliable = true;
+		PhotonNetwork.RaiseEvent(evCode, attackWithBurp, reliable, null);
+	}
+	
+	private void CustomBurpEvent(byte eventCode, object content, int senderId) {
+		if(eventCode == 0){
+			attackWithBurp = content as Attack;
+			if (attackWithBurp.startBurp)
 				MakeBurp();
 		}
 	}
-
-	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
-		if (stream.isWriting) {
-			stream.SendNext(startBurp);
-		}else{
-			startBurp = (bool)stream.ReceiveNext();
-		}
-		
-	}
-	private void TryBurp(){
+	
+	public void TryBurp() {
 		if (burp && !burp.isPlaying) {
-			startBurp = true;
+			attackWithBurp.startBurp = true;
 			MakeBurp();
 		}
 	}
 	
-	public void MakeBurp () {
-		startBurp = false;
-		burp.Play ();
+	public void MakeBurp() {
+		attackWithBurp.startBurp = false;
+		burp.Play();
 	}
 
+	//public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+	//	if (stream.isWriting) {
+	//		Debug.Log("Send bool: " + startBurp);
+	//		stream.SendNext(startBurp);
+	//	}else{
+	//		startBurp = (bool)stream.ReceiveNext();
+	//		Debug.Log("Receive bool: " + startBurp);
+	//	}		
+	//}
+	
 }
 
